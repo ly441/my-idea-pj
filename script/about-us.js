5d7d5b0ff8ca530f3e026534a3fe552b18998619
lynn_kolii <koliilynn@gmail.com>
2025-02-06 09:45:20 +0300

    linked all my htmls and js correctly

--- script/about-us.js
+++ script/about-us.js
@@ -1,4 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
+  console.log("About Page Loaded!");
 
-  console.log("Welcome to the About Us Page!");
+  
+  let moreInfo = document.querySelector("more-info");
+  let button = document.querySelector("read-more-btn");
+
+  if (button) {
+    button.addEventListener("click", function () {
+      if (moreInfo.style.display === "none" || moreInfo.style.display === "") {
+        moreInfo.style.display = "block";
+        button.innerText = "Show Less";
+      } else {
+        moreInfo.style.display = "none";
+        button.innerText = "Read More";
+      }
+    });
+  }
 });
--- script/script.js
+++ script/script.js
@@ -1,3 +1,28 @@
 
 
 
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Home Page Loaded!");
+
+    
+    let welcomeText = document.querySelector("#home-welcome");
+    if (welcomeText) {
+        let hour = new Date().getHours();
+        let greeting = "Welcome!";
+
+        if (hour < 12) {
+            greeting = "Good Morning! ";
+        } else if (hour < 18) {
+            greeting = "Good Afternoon! ";
+        } else {
+            greeting = "Good Evening! ";
+        }
+
+        welcomeText.innerText = greeting;
+    }
+
+
+
+    
+});
+
